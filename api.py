37c2a4616f0cb8c7cc16850160205dc679870ed8
Kushal Batra <24f2000859@ds.study.iitm.ac.in>
2025-11-29 00:54:59 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,11 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+SECRET = "XYZ123"   # your secret value
+
+@app.post("/quiz")
+async def quiz_endpoint(request: Request):
+    data = await request.json()
+    return {"received": data}
 
